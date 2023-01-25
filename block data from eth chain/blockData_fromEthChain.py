0b1bc8761e44f3f7c782ce50c742f5f920f71af3
Aditya Chouhan <53389950+error000002@users.noreply.github.com>
2023-01-25 14:07:19 +0530

    Add files via upload

---
+++ block data from eth chain/blockData_fromEthChain.py
@@ -0,0 +1,16 @@
+from web3 import Web3
+from flask import Flask
+from web3.middleware import geth_poa_middleware
+
+w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+print(w3)
+print(w3.isConnected())
+
+latest_block = w3.eth.get_block('latest')
+print(latest_block)
+
+print("\n")
+
+address = input("Enter Address: ")
+tx_count = Web3.eth.getTransactionCount(address)
