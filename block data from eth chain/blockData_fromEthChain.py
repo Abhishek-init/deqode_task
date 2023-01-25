dc9370575eb507a6fc0575203e14207e1503be13
Aditya Chouhan <53389950+error000002@users.noreply.github.com>
2023-01-25 14:17:23 +0530

    Update blockData_fromEthChain.py

--- block data from eth chain/blockData_fromEthChain.py
+++ block data from eth chain/blockData_fromEthChain.py
@@ -1,11 +1,13 @@
+# Import Essential lib #web3
 from web3 import Web3
-from flask import Flask
 from web3.middleware import geth_poa_middleware
 
+# Connect to an Ethereum node
 w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 print(w3)
 print(w3.isConnected())
 
+# Get the block data for the latest block
 latest_block = w3.eth.get_block('latest')
 print(latest_block)
