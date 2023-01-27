e02870dc06267763570dcf97c4447edb10da98a8
Aditya Chouhan <53389950+error000002@users.noreply.github.com>
2023-01-27 16:15:43 +0530

    Update transactionData_usingTransactionHash.py

--- Blockchain Explorer/Objectives(Completed)/Objective 3/transactionData_usingTransactionHash.py
+++ Blockchain Explorer/Objectives(Completed)/Objective 3/transactionData_usingTransactionHash.py
@@ -8,7 +8,8 @@ from web3.middleware import geth_poa_middleware
 #'0x1287bce9d76712f6410c585e1921056f1abfc98c9bb8b57680873de561160234'
 
 def get_transaction_data(transaction_hash):
-    w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
+    # w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
+    w3 = Web3(Web3/HTTPProvider('httpd://127.0.0.1:8545)
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     Transaction_data = w3.eth.get_transaction(transaction_hash)  #Using Transaction Hash
     print(Transaction_data)
