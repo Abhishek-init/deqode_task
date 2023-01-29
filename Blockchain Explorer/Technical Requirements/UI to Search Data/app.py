3b0da452f8906d15f5571eaf4a342cd16df4ca2e
Aditya Chouhan <53389950+error000002@users.noreply.github.com>
2023-01-29 13:32:26 +0530

    Update app.py

--- Blockchain Explorer/Technical Requirements/UI to Search Data/app.py
+++ Blockchain Explorer/Technical Requirements/UI to Search Data/app.py
@@ -1,21 +1,37 @@
 import json
-# from attrdict import  AttrDict
 from web3 import Web3
 from flask import Flask, jsonify
 from flask_cors import CORS
 from web3.middleware import geth_poa_middleware
 
 app = Flask(__name__)
-# CORS(app)
 CORS(app, resources={r"/api/*": {"origins": "*"}}, headers='Content-Type')
 
+w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
+if w3.isConnected():
+    print("Connected to Ethereum network")
+else:
+    print("Not connected to Ethereum network")
+
+# Fetch the balance of an Ethereum address
+# '0x3b9bA781797b57872687Ce5d5219A1A4Bc0e85ea'
+@app.route('/api/balance/<address>', methods=['GET'])
+def fetch_metamask_data(address):
+    try:
+        w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
+        # w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+        w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+        balance = w3.eth.getBalance(address)
+        return json.dumps(balance, default=str)
+    except:
+        return json.dumps("Please Enter valid Address")
 
 #Getting block data using block number
 @app.route('/api/block/number/<int:block_number>', methods=['GET'])
 def fetch_block_data_num(block_number):
     try:
-        # w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
-        w3 = Web3(Web3/HTTPProvider('httpd://127.0.0.1:8545)
+        w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
+        # w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
         w3.middleware_onion.inject(geth_poa_middleware, layer=0)
         Block_data = w3.eth.getBlock(block_number)
 
@@ -31,8 +47,8 @@ print("\n")
 @app.route('/api/block/hash/<block_number>', methods=['GET'])
 def fetch_block_data_hash(block_number):
     try:
-        # w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
-        w3 = Web3(Web3/HTTPProvider('httpd://127.0.0.1:8545)
+        w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
+        # w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
         w3.middleware_onion.inject(geth_poa_middleware, layer=0)
         Block_data = w3.eth.getBlock(block_number)
 
@@ -49,8 +65,8 @@ print("\n")
 @app.route('/api/block/transaction_hash/<transaction_hash>', methods=['GET'])
 def fetch_transaction_data(transaction_hash):
     try:
-        # w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
-        w3 = Web3(Web3/HTTPProvider('httpd://127.0.0.1:8545)
+        w3 = Web3(Web3.HTTPProvider('https://celo-mainnet.infura.io/v3/c0675c5a775e421db5430b57358c8b52'))
+        # w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
         w3.middleware_onion.inject(geth_poa_middleware, layer=0)
         Transaction_data = w3.eth.get_transaction(transaction_hash)
 
